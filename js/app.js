a94126dbd55f35438779b1f728c6a2b379213163
Shawn Stallings <oxegeneral@yahoo.com>
2015-08-26 01:19:24 -0600

    working site with full js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(function() {
 
 $('#search').on('click', function(event) {
 	event.preventDefault();
+	$('.site').empty();
 
 	var activity = $('#activity option:selected').val();
 	var city = $('#city').val();
@@ -14,10 +15,14 @@ $('#search').on('click', function(event) {
 		url: "https://outdoor-data-api.herokuapp.com/api.json?api_key=1fc0ab0d006bccd6e0fdf1856cea5b2c&q[city_cont]=" + city + '&q[activities_activity_type_name_cont]=' + activity + "&radius=1000",
 		dataType: 'jsonp',
 		jsonp: 'callback',
-		jsonpCallback: 'query'
+		jsonpCallback: 'query',
+				
 	})
 
 		.done(function(data) {
+
+
+
 			for (var i = 0; i < data.places.length; i++) {
 				console.log(data);
 
@@ -34,26 +39,31 @@ $('#search').on('click', function(event) {
 						console.log(result);
 
 							var photo = result.thumbnail;
-							var image = '<img src="' + photo + "\" class=\"photo\" alt=\"location image\">";
+							
 							var name = result.name;
 							var url = result.url;
 							var desc = result.description; 
 							
-								
-							/*if (photo == "null") {
-								image = '<p>No Image</p>'
+							
+							if (photo !== null) {
+								var image = '<img src="' + photo + "\" class=\"photo\" alt=\"location image\">";
 							}
 							else {
-								
-							}*/						
-						$table.append( '<tr class="row"><td>' + image + '</td><td class="cell"><div class="desc"><b><a href="' + url + '">' + name + '</b></a><br>' + city + ', ' + state + '<br><br>' + desc + '</div></td></tr>' );
+								var image = '<img src="noimage.png" class="photo" alt="location image">';
+							}	  
+
+						$table.append( '<tr class="row"><td>' + image + '</td><td class="cell"><div class="desc"><b><a href="' + url + '">' + name + '</b></a><span> (Click name for more details)</span><br>' + city + ', ' + state + '<br><br>' + desc + '</div></td></tr>' );
 						$('.site').append($table);
 
-						
 					}
 				}
 			}
 		})
+		.fail(function() { 
+			alert("There are not results for your search.");
+
+		})
 })
 
 });
+
